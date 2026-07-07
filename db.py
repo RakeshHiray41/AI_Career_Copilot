0ec26fb941fb74dbf9ac85b97bd47fbe3a1b4777
RakeshHiray41 <rakeshhiray2002@gmail.com>
2026-07-07 13:59:08 +0530

    Add Procfile and runtime.txt

--- db.py
+++ db.py
@@ -6,14 +6,15 @@ from sqlalchemy.orm import sessionmaker, declarative_base
 load_dotenv()
 
 DATABASE_URL = os.getenv("DATABASE_URL")
-SSL_CERT_PATH = os.getenv("SSL_CERT_PATH")
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+SSL_CERT_PATH = os.path.join(BASE_DIR, "isrgrootx1.pem")
 
 engine = create_engine(
     DATABASE_URL,
     pool_pre_ping=True,
     connect_args={
         "ssl": {
-            "ca": "isrgrootx1.pem"
+            "ca": SSL_CERT_PATH
         }
     }
 )
